ceb3164af0a2b8e3d2c92b32f7967348575ca94d
swhp <sukma.wardana@mail.com>
2017-03-08 06:47:51 +0700

    create JAX RS Configuration

---
+++ src/main/java/id/swhp/javaee/guestbook/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package id.swhp.javaee.guestbook;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Sukma Wardana
+ * @since 1.0.0
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    // nothing to configure here
+}
